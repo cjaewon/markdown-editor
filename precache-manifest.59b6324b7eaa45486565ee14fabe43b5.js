cb4c7b4a7f5363fe7092c2986a0fdcbdc01094d8
cjaewon <jwn4492@outlook.com>
2020-06-14 22:09:48 +0900

    Updates

---
+++ precache-manifest.59b6324b7eaa45486565ee14fabe43b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae41f2946eefaa3c5cb50a1fd33fa0a",
+    "url": "/markdown-editor/index.html"
+  },
+  {
+    "revision": "55c8c679663d38bfb269",
+    "url": "/markdown-editor/static/css/2.72a70808.chunk.css"
+  },
+  {
+    "revision": "698df7185df952c75713",
+    "url": "/markdown-editor/static/css/main.a9c5665c.chunk.css"
+  },
+  {
+    "revision": "55c8c679663d38bfb269",
+    "url": "/markdown-editor/static/js/2.4d228669.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/markdown-editor/static/js/2.4d228669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "698df7185df952c75713",
+    "url": "/markdown-editor/static/js/main.d1d2130b.chunk.js"
+  },
+  {
+    "revision": "2824febf435ba135d962",
+    "url": "/markdown-editor/static/js/runtime-main.ccaab313.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/markdown-editor/precache-manifest.3cd078c6e4c9154daabec2b559a5b4ea.js"
+  "/markdown-editor/precache-manifest.59b6324b7eaa45486565ee14fabe43b5.js"
 );
 
 self.addEventListener('message', (event) => {
