babe22b6573d2c78cb1a8082413fc4131856fdfd
cjaewon <jwn4492@outlook.com>
2020-06-14 22:25:55 +0900

    Update README.md

--- src/index.tsx
+++ src/index.tsx
@@ -28,5 +28,5 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workersㅊ: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
